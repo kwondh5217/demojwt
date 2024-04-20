7c6f482cb65f394f2f182885ef5aa3c9ebf3b97c
kwondh5217 <trewq231@naver.com>
2024-04-20 23:00:17 +0900

    tutorial jwt

--- src/main/java/com/example/demojwt/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/example/demojwt/jwt/JwtAuthenticationEntryPoint.java
@@ -14,6 +14,6 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
